c89313322e8467e75d95f7ad660fa718f6bc7064
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-12-01 23:48:21 +0000

    add styling for MarioBros

--- src/index copy.js
+++ src/index copy.js
@@ -3,12 +3,15 @@
 // taken from phaser 3 tutorial // https://phaser.io/tutorials/making-your-first-phaser-3-game/part1
 import Phaser from 'phaser';
 // we are using WEBPACK to we HAVE TO IMPORT FILES like this
-import sky from "./assets/mbBrickBG.jpg"
-import ground from "./assets/mbBrickS.png"
-import floor from "./assets/mbBrick.png"
-import star from "./assets/superMushroomSml.png"
-import bomb from "./assets/enemyB.png"
-import monkey from "./assets/broMBb.png"
+import sky from "./assets/sky.png"
+// import ground from "./assets/platform.png"
+import ground from "./assets/platformClif.png"
+// import star from "./assets/star.png"
+import star from "./assets/bean.png"
+// import bomb from "./assets/bomb.png"
+import bomb from "./assets/booBomb1.png"
+// import monkey from "./assets/dude.png"
+import monkey from "./assets/dude1.png"
 
 class MyGame extends Phaser.Scene {
     constructor() {
@@ -19,13 +22,21 @@ class MyGame extends Phaser.Scene {
         //BECAUSE OF WEB PACK we cannot add the directory === WE HAVE TO USE THE IMPORT KEY
         this.load.image('sky', sky)
         this.load.image('ground', ground)
-        this.load.image('floor', floor)
         this.load.image('star', star)
         this.load.image('bomb', bomb)
+        //sprite sheet is loaded differently
+        // this.load.spritesheet("dude", monkey, {
+        //     frameWidth: 32,
+        //     frameHeight: 48,
+        // });
+        // this.load.spritesheet("dudeChill", monkey, {
+        //     frameWidth: 54, //53
+        //     frameHeight: 90,
+        // });
 
         this.load.spritesheet("dude", monkey, {
-            frameWidth: 50, //34
-            frameHeight: 60,
+            frameWidth: 55, //55
+            frameHeight: 90,
         });
     }
 
@@ -34,17 +45,14 @@ class MyGame extends Phaser.Scene {
         this.add.image(400, 300, "sky")
         // create platforms, they need physics so that we can stand and walk on them and the stars and bombs will also COLLIDE with the Platforms
         const platforms = this.physics.add.staticGroup()
-        const floorPlatform = this.physics.add.staticGroup()
-        floorPlatform.create(400, 558, "floor")
 
         //PLATFORMS // this is where you can generate new levels
-        // platforms.create(400, 568 height, "image").setScale(2).refreshBody()
-        platforms.create(50, 200, "ground").setScale(1).refreshBody()
-        platforms.create(250, 280, "ground").setScale(1).refreshBody()
-        platforms.create(450, 100, "ground").setScale(1).refreshBody()
+        //(x, y, "image")
+        // platforms.create(400, 568, "ground").setScale(2).refreshBody()
+        platforms.create(400, 660, "ground").setScale(9).refreshBody()
 
-        platforms.create(560, 395, "ground").setScale(1).refreshBody()
-        platforms.create(110, 430, "ground").setScale(1).refreshBody()
+        platforms.create(520, 395, "ground").setScale(2).refreshBody()
+        platforms.create(90, 270, "ground").setScale(3).refreshBody()
         platforms.create(750, 220, "ground").setScale(1).refreshBody()
 
         //this is for the player with this.player == we are making it a property of the CLASS MyGame== do this because we also want to use t in update() === GLOBAL TO THE CLASS
@@ -56,31 +64,60 @@ class MyGame extends Phaser.Scene {
         this.player.setCollideWorldBounds(true);
         // collider allows the platforms to be solid for player
         this.physics.add.collider(this.player, platforms)
-        this.physics.add.collider(this.player, floorPlatform)
 
         //ANIMATIONS
         //idle
         this.anims.create({
             key: "turn",
-            frames: [{ key: "dude", frame: 0 }],
+            //"dude holds all frames, 4 is the middle idle frame"
+            frames: [{ key: "dude", frame: 2 }],
+            // frames: [{ key: "dude", frame: 10 }],
             frameRate: 20,
         })
         //right walking
         this.anims.create({
             key: "right",
-            frames: this.anims.generateFrameNumbers("dude", { start: 1, end: 2 }),
+            frames: [{ key: "dude", frame: 10 }],
             frameRate: 10,
             repeat: -1,
         })
         //left walking
         this.anims.create({
             key: "left",
-            frames: this.anims.generateFrameNumbers("dude", { start: 1, end: 2 }),
+            frames: [{ key: "dude", frame: 8 }],
             frameRate: 10,
             //noRepeat
             repeat: -1,
         })
-
+        //left walking
+        // this.anims.create({
+        //     key: "up",
+        //     frames: [{ key: "dude", frame: 10 }],
+        //     frameRate: 10,
+        //     repeat: -1,
+        // })
+        // //idle
+        // this.anims.create({
+        //     key: "turn",
+        //     //"dude holds all frames, 4 is the middle idle frame"
+        //     frames: [{ key: "dude", frame: 4 }],
+        //     frameRate: 20,
+        // })
+        // //right walking
+        // this.anims.create({
+        //     key: "right",
+        //     frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        //     frameRate: 10,
+        //     repeat: -1,
+        // })
+        // //left walking
+        // this.anims.create({
+        //     key: "left",
+        //     frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        //     frameRate: 10,
+        //     //noRepeat
+        //     repeat: -1,
+        // })
         //STARS
         //this does not have to be a GLOBAL VARIABLE === can use CONST
         const stars = this.physics.add.group({
@@ -90,14 +127,13 @@ class MyGame extends Phaser.Scene {
             // want them to be spawned spread out throughout the game == perfectly spaced out by 70px
             setXY: { x: 12, y: 0, stepX: 70 },
         })
-        // selects all children stars
+        // sellects all children stars
         stars.children.iterate(function (child) {
             // give them all a random bounce between o.4 and 0.8
             child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
         })
         // stars cannot go through platforms
         this.physics.add.collider(stars, platforms)
-        this.physics.add.collider(stars, floorPlatform)
         //overlap == overlaps the player and the stars == as player walks over them the get collected
         // collect is actually a function! 
         this.physics.add.overlap(this.player, stars, collect, null, this)
@@ -106,7 +142,6 @@ class MyGame extends Phaser.Scene {
         //add group?
         const bombs = this.physics.add.group()
         this.physics.add.collider(bombs, platforms)
-        this.physics.add.collider(bombs, floorPlatform)
         //bombTouched=== function
         this.physics.add.collider(this.player, bombs, bombTouched, null, this)
         //function == gravity stops and greys out and ends turn === END GAME
@@ -118,8 +153,8 @@ class MyGame extends Phaser.Scene {
 
         //SCORE TEXT
         // this creates the score board
-        const scoreText = this.add.text(30, 30, "score: 0", {
-            frontSize: "45px",
+        const scoreText = this.add.text(15, 15, "score: 0", {
+            frontSize: "32px",
             fill: "#000"
         })
         // global var to start collection at 0
@@ -127,7 +162,7 @@ class MyGame extends Phaser.Scene {
 
         //STARS COLLISION
         //when collect function runs it is going to say when the player overlaps the stars === do this function
-        // takes player and star parameter === disableBody === takes it off the screen
+        // takes player and star parameter === disableBody === takes it opff the screen
         //disableBody?
         function collect(player, star) {
             star.disableBody(true, true)
@@ -172,7 +207,7 @@ class MyGame extends Phaser.Scene {
             this.player.setVelocityX(160);
             this.player.anims.play("right", true);
             this.player.flipX = false
-        } else {
+        }  else {
             //this stops this continued walking
             this.player.setVelocityX(0)
             this.player.anims.play("turn")
@@ -182,7 +217,6 @@ class MyGame extends Phaser.Scene {
         if (cursors.up.isDown && this.player.body.touching.down) {
             //(higher the jump to clear the platforms)
             this.player.setVelocityY(-420)
-            this.player.anims.play("up", true);
             // this.player.anims.play("up", true);
         }
     }
